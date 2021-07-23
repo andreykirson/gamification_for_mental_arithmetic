3db9c31a66907cdc5a1a81c23c6c871dc45d9fab
andrey.tazov <andrey.tazov@g5e.com>
2021-07-23 22:41:06 +0300

    starting with microservices

---
+++ src/main/java/com/example/gamification/game/BadgeCard.java
@@ -0,0 +1,28 @@
+package com.example.gamification.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BadgeCard {
+    @Id
+    @GeneratedValue
+    private Long badgeId;
+    private Long userId;
+    @EqualsAndHashCode.Exclude
+    private long badgeTimestamp;
+    private BadgeType badgeType;
+
+    public BadgeCard(final Long userId, final BadgeType badgeType) {
+        this(null, userId, System.currentTimeMillis(), badgeType);
+    }
+}
